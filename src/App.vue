4d25d554fa841c165e2eee289c790a836d5b6227
HoBager <chel43@inbox.ru>
2023-09-15 18:06:20 +0500

    add notification

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <main>
-    <div class="inventory">
-      <v-info />
-      <inventory-body />
+    <div class="page">
+      <div class="inventory">
+        <v-info />
+        <inventory-body />
+      </div>
+      <notification-window />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
 import InventoryBody from './components/inventory-body.vue'
+import NotificationWindow from './components/notification-window.vue'
 import vInfo from './components/v-info.vue'
 </script>
 
 <style scoped lang="scss">
+main {
+  display: flex;
+  justify-content: center;
+}
+.page {
+  display: flex;
+  flex-direction: column;
+}
 .inventory {
   display: flex;
   gap: 24px;
--- src/components/notification-window.vue
+++ src/components/notification-window.vue
@@ -1,11 +1,39 @@
 <template>
-  <div class="notification">
+  <div v-if="isOpen" class="notification">
     <text-skeleton width="700px" height="36px" radius="12px" />
+    <button @click="isOpen = false" class="close"><img src="carbon-close.svg" /></button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import textSkeleton from './text-skeleton.vue'
+
+const isOpen = ref<boolean>(true)
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.notification {
+  position: relative;
+  max-width: 785px;
+  height: 72px;
+  padding: 18px;
+  border-radius: 12px;
+  border: 1px solid $primary;
+  background: $secondary;
+  margin-top: 24px;
+  .close {
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: unset;
+    border: none;
+    cursor: pointer;
+  }
+}
+</style>
